bea809a13f745e889e8ce091fad3df4e7c2f1ac8
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2023-03-28 08:29:24 +0530

    upgraded to spring boot 3

--- src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
+++ src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
@@ -1,12 +1,8 @@
 package net.javaguides.springboot.tutorial.entity;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Student {
